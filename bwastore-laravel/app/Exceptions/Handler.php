86ca930ed811321ee2dbb6b536a55d1450676350
lionnie <lionnieaprila@gmail.com>
2021-11-02 00:23:13 +0700

    Database Aplikasi Laravel_BWAStoreFullstack
    Baru sampai membuat database user

--- bwastore-laravel/app/Exceptions/Handler.php
+++ bwastore-laravel/app/Exceptions/Handler.php
@@ -16,24 +16,6 @@ class Handler extends ExceptionHandler
         //
     ];
 
-    /**
-     * A list of the inputs that are never flashed for validation exceptions.
-     *
-     * @var array
-     */
-    protected $dontFlash = [
-        'password',
-        'password_confirmation',
-    ];
-
-    /**
-     * Report or log an exception.
-     *
-     * @param  \Throwable  $exception
-     * @return void
-     *
-     * @throws \Exception
-     */
     public function report(Throwable $exception)
     {
         if (app()->bound('sentry') && $this->shouldReport($exception)) {
@@ -41,5 +23,28 @@ class Handler extends ExceptionHandler
         }
 
         parent::report($exception);
+    }
+    
+    /**
+     * A list of the inputs that are never flashed for validation exceptions.
+     *
+     * @var array
+     */
+    protected $dontFlash = [
+        'current_password',
+        'password',
+        'password_confirmation',
+    ];
+
+    /**
+     * Register the exception handling callbacks for the application.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // $this->reportable(function (Throwable $e) {
+        //     //
+        // });
     }
 }
